c2fe412d59039de48628e623fa63a0bcb7364406
amit <naveen.kumar@truringsoftek.com>
2020-08-08 14:52:42 +0530

    changes

---
+++ src/config/postgresql.js
@@ -0,0 +1,19 @@
+
+const escape= require('pg-escape')
+const { Pool, Client } = require('pg')
+const connectionPool = new Pool({
+  host: `localhost`,
+  user: `amit`,
+  password: `password@2020`,
+  database: `db`,
+  port: 5432
+})
+
+
+
+connectionPool.connect().then(
+    module.exports = {
+            connectionPool: connectionPool,
+            escape: escape
+        }
+    ).catch((err) => {throw new Error(err)});
